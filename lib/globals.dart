51470bcb544e7d7058f2720d74f594f5664ad9ce
viktor <ashenarx@gmail.com>
2022-10-22 17:42:15 +1000

    Add globals

---
+++ lib/globals.dart
@@ -0,0 +1,11 @@
+library my_prj.globals; //Объявляем его как либу.
+//Список глобальных переменных с значениями по умолчанию.
+// asset path
+
+const String profileImage = 'assets/images/theKlenin.jpeg';
+const String atbLogo = 'assets/svg/atb_logo-orange_black.svg';
+
+bool AllOK = false;
+int USERID = 0;
+String somewonetext = "LOL";
+
