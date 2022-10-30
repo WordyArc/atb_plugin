c843a77da9182c487223519a40bf67ade747bcdb
viktor <ashenarx@gmail.com>
2022-10-31 00:38:55 +1000

    Update profile_card: refactoring

--- lib/ui/widgets/profile_card.dart
+++ lib/ui/widgets/profile_card.dart
@@ -4,16 +4,18 @@ import 'package:flutter_svg/svg.dart';
 
 class ProfileCard extends StatelessWidget {
   final String title;
-  final String subtitle;
   final String pathToIcon;
   final VoidCallback onPressed;
 
-  static const double _iconSize = 50.0;
+  static const Color _cardBackgroundColor = Color(0xFFE8E8E8);
+  static const double _leadingSize = 30.0;
+  static const String _trailingPath = 'assets/svg/chevron_down.svg';
+  static const double _trailingSize = 24.0;
+  static const Color _trailingColor = Color(0xFFA9A9A9);
 
   const ProfileCard({
     Key? key,
     required this.title,
-    required this.subtitle,
     required this.pathToIcon,
     required this.onPressed,
   }) : super(key: key);
@@ -22,35 +24,35 @@ class ProfileCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          ListTile(
-            onTap: onPressed,
-            leading: ConstrainedBox(
-              constraints: const BoxConstraints(
-                minWidth: _iconSize,
-                minHeight: _iconSize,
-                maxWidth: _iconSize,
-                maxHeight: _iconSize,
-              ),
-              child: SvgPicture.asset(
-                  pathToIcon,
-                  color: atbMainColor,
-              ),
-            ),
-            title: Text(
-              title,
-              style: const TextStyle(fontWeight: FontWeight.bold),
-            ),
-            trailing: Container(
-              width: 5,
-              color: Colors.grey,
-            ),
-            subtitle: Text(subtitle),
+      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 7),
+      child: Container(
+        decoration: BoxDecoration(
+          color: _cardBackgroundColor,
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: ListTile(
+          onTap: onPressed,
+          leading: SvgPicture.asset(
+              pathToIcon,
+              color: Colors.black,
+              width: _leadingSize,
+            height: _leadingSize,
           ),
-        ],
+          title: Text(
+            title,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          trailing: SvgPicture.asset(
+            _trailingPath,
+            color: _trailingColor,
+            width: _trailingSize,
+            height: _trailingSize,
+          ),
+          visualDensity: const VisualDensity(vertical: 1),
+        ),
       ),
     );
   }
